255f45f5ea86aacd9a84a13212fc30ce70680e3f
Yousaf Ramzan <yousaf0811@gmail.com>
2022-10-31 13:55:46 +0500

    monday update

--- Lab Work/validate.js
+++
@@ -1,7 +0,0 @@
-function validity(){
-    var name = document.getElementsByClassName('num1').value;
-    var phone = document.getElementsByClassName('num2').value;
-    var cnic = document.getElementsByClassName('num3').value;
-    var age = document.getElementsByClassName('num4').value;
-    console.log(name);
-}
